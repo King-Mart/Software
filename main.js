c2f508cb0824fb779e1c0b99e61dc510f65905ab
Filip Popadich <fpopa104@uottawa.ca>
2024-10-27 12:22:46 -0400

    Fixed the navbar so that it animates on scroll. I recommend that if you like the concept that you apply it to your page.

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+// this function animates the navbar sliding down once the user scrolls down
+//the navbar will hide itself once the user reaches the top of the page
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        document.getElementById("top-navbar").style.top = "0";
+    }else{
+        document.getElementById("top-navbar").style.top = "-60px";
+    }
+}
+
 var a = 1;
 var active = document.getElementsByClassName("active")
 
 function change_name(){
     active[0].innerHTML = "Hello";
 }
+
