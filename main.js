b0a4929c1dd6c2774aa1efcceda7be5ba1ea23f3
Sheila <sieyojisheila@gmail.com>
2024-10-27 20:48:49 -0400

    tried to fix the checkbox issue in pricing page

--- main.js
+++ main.js
@@ -46,14 +46,17 @@ init(); // Call the init function to start the slider
 
 //switch between annual and monthly prices in pricing page when user clicks on the checkbox
 const checkbox = document.getElementById('checkbox');
-const pro = document.getElementById('pro');
-const advanced = document.getElementById('advanced');
-const basic = document.getElementById('basic');
+const proPrice = document.getElementById('#pro.price');
+const advancedPrice = document.getElementById('#advanced.price');
+const basicPrice = document.getElementById('#basic.price');
+
+
 
 checkbox.addEventListener('click', () => {
-    basic.textContent = basic.textContent === '$99.99' ? '$9.99' : '$99.99';
-    advanced.textContent = advanced.textContent === '$199.99' ? '$14.99' : '$199.99';
-    pro.textContent = pro.textContent === '$299.99' ? '$24.99' : '$299.99';
+    basicPrice.textContent = basicPrice.textContent === '$99.99' ? '$9.99' : '$99.99';
+    advanced.textContent = advancedPrice.textContent === '$199.99' ? '$14.99' : '$199.99';
+    pro.textContent = proPrice.textContent === '$299.99' ? '$24.99' : '$299.99';
+
 
 
 });
