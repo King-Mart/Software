8f5f174223d3e77bc76299a07464f70f6a500e0a
Filip Popadich <fpopa104@uottawa.ca>
2024-11-09 22:32:42 -0500

    changed a bit of css on features. created new js file. Seems to have slightly fixed the checkbox on the pricing page.

---
+++ pricing.js
@@ -0,0 +1,16 @@
+//switch between annual and monthly prices in pricing page when user clicks on the checkbox
+const checkbox = document.getElementById('checkbox');
+const pro = document.getElementById('pro');
+const advanced = document.getElementById('advanced');
+const basic = document.getElementById('basic');
+
+
+
+checkbox.addEventListener('click', () => {
+    basic.textContent = basic.textContent === '$99.99' ? '$9.99' : '$99.99';
+    advanced.textContent = advanced.textContent === '$199.99' ? '$14.99' : '$199.99';
+    pro.textContent = pro.textContent === '$299.99' ? '$24.99' : '$299.99';
+
+
+
+});
